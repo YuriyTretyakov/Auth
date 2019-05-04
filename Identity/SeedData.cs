b776b4e142be355ec947f40e89f4151ecea9b796
YuriyTretyakov <pokusun.yt@gmail.com>
2019-05-04 09:35:20 +0300

    Add files via upload

---
+++ Identity/SeedData.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Authorization.Identity
+{
+    public class AuthDataSeeder
+    {
+        private readonly AuthDbContext _context;
+        private readonly UserManager<User> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        private const string _seedUserName = "rootuser@authenticationservice.com";
+
+        public AuthDataSeeder(AuthDbContext context, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public async Task SeedUsers()
+        {
+            if (!await _roleManager.RoleExistsAsync("Administrator"))
+                await _roleManager.CreateAsync(new IdentityRole { Name = "Administrator" });
+
+            if (await _userManager.FindByEmailAsync(_seedUserName) == null)
+            {
+                var user = new User
+                {
+                    UserName = _seedUserName,
+                    Email = _seedUserName
+                };
+
+                IdentityResult result = await _userManager.CreateAsync(user, "P@ssword");
+
+                if (result.Succeeded)
+                    await _userManager.AddToRolesAsync(user, new[] { "Administrator"});
+            }
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Authorization.Identity;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
+
+namespace Authorization
+{
+    public class Startup
+    {
+        private readonly IConfigurationRoot _config;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            var builder = new ConfigurationBuilder().AddJsonFile("appsettings.json");
+            _config = builder.Build();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            c.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info { Title="My cool swagger",Version="1.0" })
+            );
+
+            services.AddMvc().AddJsonOptions(
+                config =>
+                {
+                    config.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+                }
+                );
+            services.AddSingleton<IConfiguration>(_config);
+            services.AddDbContext<AuthDbContext>();
+            services.AddTransient<AuthDataSeeder>();
+           
+
+
+            services.AddIdentity<User, IdentityRole>(config =>
+            {
+                config.User.RequireUniqueEmail = true;
+                config.Password.RequiredLength = 4;
+                config.Password.RequireDigit = false;
+
+            }).AddDefaultTokenProviders()
+              .AddEntityFrameworkStores<AuthDbContext>();
+            services.AddLogging();
+
+            JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear(); // => remove default claims
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+                })
+                .AddJwtBearer(cfg =>
+                {
+                    cfg.Authority = "https://sts.windows.net/";
+                    cfg.RequireHttpsMetadata = false;
+                    cfg.SaveToken = true;
+                    cfg.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = Configuration["JwtIssuer"],
+                        ValidAudience = Configuration["JwtIssuer"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JwtKey"])),
+                        ClockSkew = TimeSpan.Zero // remove delay of token when expire
+                    };
+                   
+                });
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env,AuthDataSeeder seedData)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c=>
+            {
+                c.SwaggerEndpoint("/swagger", "Swagger Sample");
+            });
+
+            app.UseHttpsRedirection();
+            app.UseMvc(config =>
+            {
+                config.MapRoute(
+                    name: "Default",
+                    template: "{controller}/{action}/{id?}",
+                    defaults: new { controller = "App", action = "Index" }
+                );
+
+            });
+
+            seedData.SeedUsers().Wait();
+        }
+    }
+}
